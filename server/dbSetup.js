f066bebb0f28f5b54cb707d06eaa8cc3ffbbbdec
Kemane Wright <Kemane.Wright25@gmail.com>
2017-05-18 14:29:04 -0600

    .

--- server/dbSetup.js
+++ server/dbSetup.js
@@ -11,11 +11,10 @@ var log = function(input) {
 
 module.exports = {
   run: function() {
-    // console.log('Initializing Database');
-
+    console.log('Initializing Database');
     db.build_tables(function(err, table) {
-      // if (err) return console.log('Error loading tables', err);
-      // else console.log('Tables loaded')
+      if (err) return console.log('Error loading tables', err);
+      else console.log('Tables loaded')
     })
   }
 
