e8f1d6e6fefe18e75cf4521437eb617cdd68a06a
Kemane Wright <Kemane.Wright25@gmail.com>
2017-05-18 14:33:02 -0600

    .

---
+++ server/config.js
@@ -0,0 +1,8 @@
+const port = 2469
+
+module.exports = {
+  PORT: port,
+  MASSIVE_URI: '	postgres://kdoximcd:uRsg3fs3ngzP37HYnuqPj3ppnCIUbJQN@stampy.db.elephantsql.com:5432/kdoximcd',
+  SESSION_SECRET: 'gameCollectionSecret',
+  INITALIZE_LOG: true
+}
